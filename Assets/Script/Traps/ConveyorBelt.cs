19dcacba6a69aa04f1f7d197d92204048372b4ab
Clément THOMAS <clemto95@live.fr>
2023-10-01 15:17:06 +0200

    add coveyor belt

--- Assets/Script/Traps/ConveyorBelt.cs
+++ Assets/Script/Traps/ConveyorBelt.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class ConveyorBelt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject player;
+    [SerializeField] float conveyorBeltSpeed;
+    [SerializeField] bool goUp;
+    [SerializeField] bool goDown;
+    [SerializeField] bool goRight;
+    [SerializeField] bool goLeft;
+    Rigidbody2D rb;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.gameObject == player)
+        {
+            //player.GetComponent<PlayerController>().enabled = false;
+            rb = player.GetComponent<Rigidbody2D>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (collision.gameObject == player)
+        {
+            if (goUp)
+            {
+                rb.AddForce(Vector2.up * conveyorBeltSpeed);
+            }
+            else if (goDown)
+            {
+                rb.AddForce(-Vector2.up * conveyorBeltSpeed);
+            }  
+            else if (goRight)
+            {
+                rb.AddForce(Vector2.right * conveyorBeltSpeed);
+            }
+            else if (goLeft)
+            {
+                rb.AddForce(-Vector2.right * conveyorBeltSpeed);
+            }
+        }
     }
+
+    /*private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }*/
 }
