4168af0087aace78bee48edfbf9e1054a615466a
Haiito92 <hannaantoinebusiness@gmail.com>
2023-10-02 12:34:46 +0200

    [ADD] Traps tiles
    Goudron, void
    Animation Drone

--- Assets/Script/Traps/Holes.cs
+++ Assets/Script/Traps/Holes.cs
@@ -6,7 +6,7 @@ public class Holes : MonoBehaviour
 {
     [SerializeField]
     GameObject player;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject == player)
         {
