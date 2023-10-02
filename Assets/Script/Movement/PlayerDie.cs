ac277ada4b01ebfd9188b082e35933f015ae50af
Clément THOMAS <clemto95@live.fr>
2023-10-02 16:45:38 +0200

    Add checkpoint

--- Assets/Script/Movement/PlayerDie.cs
+++ Assets/Script/Movement/PlayerDie.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class PlayerDie : MonoBehaviour
 {
+    [SerializeField] private GameObject _actualCheckpoint;
+    [SerializeField] private GameObject _previousCheckpoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Checkpoint")
+        {
+            if(_actualCheckpoint != null)
+            {
+                _previousCheckpoint = _actualCheckpoint;
+                _previousCheckpoint.SetActive(true);
+            }
+            _actualCheckpoint = collision.gameObject;
+            collision.gameObject.SetActive(false);
+        }
+    }
+
     public void Die()
     {
-        Destroy(gameObject);
+        transform.position = _actualCheckpoint.transform.position;
     }
 }
