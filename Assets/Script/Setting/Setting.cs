a50c63c3de56b14f6e3957d7965eefb0080329de
Flavio-Leon <flavio.leon2004@gmail.com>
2023-10-02 09:27:37 +0200

    {Add} Sound setting script
    Sound script

--- Assets/Script/Setting/Setting.cs
+++ Assets/Script/Setting/Setting.cs
@@ -6,32 +6,44 @@ using System.Collections.Generic;
 
 public class Setting : MonoBehaviour
 {
-    public TMP_Dropdown resolutionDropdown;
-    Resolution[] resolutions;
+    [SerializeField] TMP_Dropdown _resolutionDropdown;
+    private Resolution[] _resolutions;
+    [SerializeField] private AudioMixer _myMixer;
+    [SerializeField] private Slider _audioMusic;
+    [SerializeField] private Slider _audioSound;
+    [SerializeField] private Slider _audioGeneral;
 
     public void Start()
     {
-        resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
+        _resolutions = Screen.resolutions;
+        _resolutionDropdown.ClearOptions();
 
         List<string> options = new List<string>();
 
         int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < _resolutions.Length; i++)
         {
-            string option = resolutions[i].width + "x" + resolutions[i].height;
+            string option = _resolutions[i].width + "x" + _resolutions[i].height;
             options.Add(option);
 
-            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            if (_resolutions[i].width == Screen.width && _resolutions[i].height == Screen.height)
             {
                 currentResolutionIndex = i;
             }
         }
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
+        
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+           LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+        }
 
-        Screen.fullScreen = true;
     }
 
     public void SetFullscreen(bool isFullscreen)
@@ -41,8 +53,36 @@ public class Setting : MonoBehaviour
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = _resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = _audioMusic.value;
+        _myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void SetSFXVolume()
+    {
+        float volume = _audioSound.value;
+        _myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    public void SetGeneralVolume()
+    {
+        float volume = _audioGeneral.value;
+        _myMixer.SetFloat("General", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("GeneralVolume", volume);
+    }
+
+    public void LoadVolume()
+    {
+        _audioMusic.value = PlayerPrefs.GetFloat("musicVolume");
+        _audioSound.value = PlayerPrefs.GetFloat("soundVolume");
+        _audioGeneral.value = PlayerPrefs.GetFloat("generalVolume");
+
+        SetMusicVolume();
     }
 
 }
